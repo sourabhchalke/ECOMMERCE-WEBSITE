e86a696a8f251c05c1bf5ce6cd7333acab825a9f
sourabhchalke <sourabhchalke2157@gmail.com>
2025-01-29 17:12:53 +0530

    left-side of modal[signIn page] is created

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -1,20 +1,60 @@
 import React from "react";
-import styled from 'styled-components';
-import Modal from '@mui/material/Modal';
+import styled from "styled-components";
+import Modal from "@mui/material/Modal";
+import LogoImg from '../utils/Images/logo2.png';
+import AuthImg from '../utils/Images/AuthImage.png';
 
-const Container = styled.div``;
-const Left = styled.div``;
-const Right = styled.div``;
+const Container = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+`;
+const Left = styled.div`
+  flex: 1;
+  position: relative;
+  @media screen and (max-width : 768px){
+  display:none;
+`;
+const Logo=styled.img`
+  width:20%;
+  position:absolute;
+  left:10px;
+  top:10px;
+  z-index:10;
+`;
+const Image=styled.img`
+  position:relative;
+  height:100%;
+  width:100%;
+  object-fit:cover;
+`;
 
-const Authentication=({openAuth,setOpenAuth})=> {
+const Right = styled.div`
+  flex: 1;
+  position: relative;
+  background: ${({ theme }) => theme.black};
+  display:flex;
+  flex-direction:column;
+  padding:40px;
+  gap:16px;
+  align-items:center;
+  justify-content:center;
+
+`;
+
+const Authentication = ({ openAuth, setOpenAuth }) => {
   return (
-    <Modal open={openAuth} onClose={()=>setOpenAuth(false)}>
+    <Modal open={openAuth} onClose={() => setOpenAuth(false)}>
       <Container>
-        <Left>L</Left>
+        <Left>
+          <Logo src={LogoImg}/>
+          <Image src={AuthImg}/>
+        </Left>
         <Right>R</Right>
       </Container>
     </Modal>
   );
-}
+};
 
 export default Authentication;
