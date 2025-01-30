8d0101a3696b4ab91656467a432cc284800e8df8
sourabhchalke <sourabhchalke2157@gmail.com>
2025-01-30 19:02:50 +0530

    Header page added to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import homeImg from "../utils/Images/Header.png";
+
+// Css
+const Container = styled.div`
+  padding: 20px 20px;
+  height: 100%;
+  width:100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  @media (max-width: 768px) {
+    padding: 20px 12px;
+  }
+  background: ${({ theme }) => theme.bg};
+`;
+const Section = styled.div`
+ 
+`;
+const Img = styled.img`
+  width: 100%;
+  height: 700px;
+  object-fit: cover;
+`;
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  )
+    <Container>
+      <Section>
+        <Img src={homeImg} />
+      </Section>
+    </Container>
+  );
 }
 
 export default Home;
