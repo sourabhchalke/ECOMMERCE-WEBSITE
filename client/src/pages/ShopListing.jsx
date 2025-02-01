882b27dfd90f495084f6e827774abb6161b13eb0
sourabhchalke <sourabhchalke2157@gmail.com>
2025-02-01 16:19:33 +0530

    Products added to ShopList

--- client/src/pages/ShopListing.jsx
+++ client/src/pages/ShopListing.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import ProductCard from '../components/cards/ProductCard';
+import { filter } from '../utils/data';
+
+const Container=styled.div`
+  height:100%;
+  // width:100%;
+  border:1px solid red;
+  display:flex;
+  align-items:center;
+  // justify-content:center;
+  background:${({theme})=> theme.bg};
+
+  @media (max-width:768px){
+    flex-direction:column;
+  }
+
+`;
+const Filters=styled.div`
+  width:230px;
+  height:90%;
+  overflow-y:scroll;
+  padding:20px 16px;
+`;
+const Menu=styled.div`
+  display:flex;
+  flex-direction:column;
+  gap:4px;
+`;
+const FilterSection=styled.div``;
+const Title=styled.div``;
+const Products=styled.div``;
+const CardWrapper=styled.div``;
+
 
 function ShopListing() {
   return (
-    <div>
-      <h2>Shop</h2>
-    </div>
+    <Container>
+      <Filters>
+        <Menu>
+
+         
+
+        </Menu>
+      </Filters>
+      <Products>
+        <CardWrapper>
+          <ProductCard/>
+        </CardWrapper>
+      </Products>
+    </Container>
   )
 }
 
