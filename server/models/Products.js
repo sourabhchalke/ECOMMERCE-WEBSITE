145cf7070a51d92968db40b254bcd16c4c1dc9f3
sourabhchalke <sourabhchalke2157@gmail.com>
2025-02-05 16:00:51 +0530

    Product Schema added to Products.js file

---
+++ server/models/Products.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    desc:{
+        type:String,
+        required:true,
+    },
+    img:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type: {
+            org: { type: Number, default: 0.0, min: 0 },
+            mrp: { type: Number, default: 0.0, min: 0 },
+            off: { type: Number, default: 0, min: 0 } // min: 0 → Prevents negative values for price fields.
+        },
+        default:{
+            org:0.0,
+            mrp:0.0,
+            off:0,
+        }
+    },
+})
+
+export default mongoose.model("Products",ProductSchema,"Products");
