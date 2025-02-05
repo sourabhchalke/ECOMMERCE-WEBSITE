e4caa33c51412590fad2186369562d249dff1c7e
sourabhchalke <sourabhchalke2157@gmail.com>
2025-02-05 15:11:29 +0530

    Connected to MongooDB[Database] with the help of .env file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Database Connected"))
+.catch(err => console.error("Something went wrong:", err));
+
+
+app.get('/',(req,res)=>{
     res.send("Server Running");
-});
-
+})
 
 app.listen(8080,()=>{
     console.log("Server Successfully Running on Port:8080");
