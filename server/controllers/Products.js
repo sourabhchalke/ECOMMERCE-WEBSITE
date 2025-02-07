83fec45f3fab1580185d58f71a22d9c9569c2684
sourabhchalke <sourabhchalke2157@gmail.com>
2025-02-07 17:42:22 +0530

    getProduct part added for getting product by searching,filtering,etc

--- server/controllers/Products.js
+++ server/controllers/Products.js
@@ -1,20 +1,18 @@
 const mongoose=require('mongoose');
 const ProductSchema=require('../models/Products');
+const Products = require('../models/Products');
 
 const addProduct = async(req,res)=>{
 
     try{
-
         const productsData = req.body;
         console.log(productsData);
-        
 
         if(!Array.isArray(productsData)) {
             console.log("Invalid/Misinformation Please Enter Correct Details");
         } 
         
         const createdProducts=[];
-
         const {title,name,desc,img,price,size,category}=productsData;
         console.log(title,name,desc);
 
@@ -36,7 +34,54 @@ const addProduct = async(req,res)=>{
         console.log(error);
         return res.status(400).send("Something went wrong",error);
     }
+};
 
-}
+const getProduct=async(req,res)=>{
 
-module.exports={addProduct};
+    try{
+
+        console.log(req.query);
+        let {categories,minPrice,maxPrice,sizes,search}=req.query;
+        sizes = sizes?.split(',');
+        categories = categories?.split(',');
+
+        const filter = {};
+
+        if(categories && Array.isArray(categories)){
+            filter.category = {$in : categories};
+        }
+
+        if(minPrice || maxPrice){
+            filter["price.org"]={};
+
+            if(minPrice){
+                filter["price.org"]["$gte"]= parseFloat(minPrice);
+            }
+            if(maxPrice){
+                filter["price.org"]["$lte"]= parseFloat(maxPrice);
+            }
+        }
+
+        if(sizes && Array.isArray(sizes)){
+            filter.sizes = {$in : sizes};
+        }
+
+        if(search){
+            filter.$or = [
+                {title:{$regex:new RegExp(search,"i")}},
+                {desc:{$regex:new RegExp(search,"i")}},
+            ]
+        }
+
+        const products = await ProductSchema.find(filter);
+        console.log(filter);
+        return res.status(200).send(products);
+
+    }catch(error){
+        console.log(error);
+        return res.status(400).send("Something went wrong! Please try again");
+    }
+
+};
+
+module.exports={addProduct,getProduct};
