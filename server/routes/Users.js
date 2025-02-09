e481496f8a2c8506632a20ffd8db033c53f4d2c0
sourabhchalke <sourabhchalke2157@gmail.com>
2025-02-09 12:46:23 +0530

    All User routes added successfully

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express();
-const {UserRegister,UserLogin,AddToCart,RemoveFromCart,getAllCartItems} = require('../controllers/Users');
+const {UserRegister,UserLogin,AddToCart,RemoveFromCart,getAllCartItems,getAllOrders ,PlaceOrder,AddToFavorites,RemoveFromFavorites,getUserFavourites} = require('../controllers/Users');
 
 router.post('/signup',UserRegister);
 router.post('/signin',UserLogin);
 
 // Cart
+router.get('/cart',getAllCartItems);
 router.post('/cart',AddToCart);
 router.patch('/cart',RemoveFromCart);
-router.get('/cart',getAllCartItems);
+
+// Order
+router.get('/order',getAllOrders);
+router.post('/order',PlaceOrder);
+
+// Favourites
+router.get('/favorite',getUserFavourites);
+router.post('/favorite',AddToFavorites);
+router.patch('/favorite',RemoveFromFavorites);
+
+
 
 
 
